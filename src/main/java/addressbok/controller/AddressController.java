24ae44d8bb7076d23cc06d678be1f08150e53785
xarczuz <chjunchi.chiu@gmail.com>
2020-08-20 14:24:20 +0200

    reformat code

--- src/main/java/addressbok/controller/AddressController.java
+++ src/main/java/addressbok/controller/AddressController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("address")
 public class AddressController {
